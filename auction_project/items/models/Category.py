a781193a6d1d40fbad955287c9c01d7a44fa1e14
Viktoria Verkholat <peremoga2009@gmail.com>
2025-10-13 22:02:52 +0300

    description + null=True

--- auction_project/items/models/Category.py
+++ auction_project/items/models/Category.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Category(models.Model):
     title = models.CharField(max_length=50, unique=True)
-    description = models.CharField(max_length=300)
+    description = models.CharField(max_length=300, null=True)
 
     def __str__(self):
         return self.title
